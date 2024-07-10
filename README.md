72ea525af94a97539eb4ef161af5b35cac00ba51
Eliot Cho <eliotcho99@gmail.com>
2024-07-09 23:03:11 -0400

    starting on the project

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # draggable-task-board
+
+## Description
+
+A task board application that allows a team to manage project tasks.
+Built by using HTML CSS and jQuery.
+User can create a task.
+The task can be drag the task between "To Do" "In Progress" "Done"
+
+## Installation
+
+N/A
+
+## Usage
+
+Create and organize tasks
+
+## Credits
+
+N/A
+
+## Features
+
+None
+
+## Tests
+
+Run the website online and check if there's any spelling errors or bugs\
+Page link:
+
+## Screenshots
+
+The following image shows the web application's appearance and functionality:
+Main page
+
+![](./assets/images/.png)
---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+// Retrieve tasks and nextId from localStorage
+let taskList = JSON.parse(localStorage.getItem("tasks"));
+let nextId = JSON.parse(localStorage.getItem("nextId"));
+
+// create a function to generate a unique task id
+function generateTaskId() {}
+
+// create a function to create a task card
+function createTaskCard(task) {
+  const taskCard = $("<div>")
+    .addClass("card task-card draggable my-3")
+    .attr("data-task-id", task.id);
+  const cardHeader = $("<div>").addClass("card-header h4").text(task.name);
+  const cardBody = $("<div>").addClass("card-body");
+  const cardDescription = $("<p>").addClass("card-text").text(task.type);
+  const cardDueDate = $("<p>").addClass("card-text").text(task.dueDate);
+  const cardDeleteBtn = $("<button>")
+    .addClass("btn btn-danger delete")
+    .text("Delete")
+    .attr("data-task-id", task.id);
+  cardDeleteBtn.on("click", handleDeleteTask);
+
+  // sets the card background color based on due date. Only apply the styles if the dueDate exists and the status is not done.
+  if (task.dueDate && task.status !== "done") {
+    const now = dayjs();
+    const taskDueDate = dayjs(task.dueDate, "DD/MM/YYYY");
+
+    // if the task is due today, make the card yellow. If it is overdue, make it red.
+    if (now.isSame(taskDueDate, "day")) {
+      taskCard.addClass("bg-warning text-white");
+    } else if (now.isAfter(taskDueDate)) {
+      taskCard.addClass("bg-danger text-white");
+      cardDeleteBtn.addClass("border-light");
+    }
+  }
+
+  // gather all the elements created above and append them to the correct elements.
+  cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
+  taskCard.append(cardHeader, cardBody);
+
+  // ? Return the card so it can be appended to the correct lane.
+  return taskCard;
+}
+
+// create a function to render the task list and make cards draggable
+function renderTaskList() {}
+
+// create a function to handle adding a new task
+function handleAddTask(event) {}
+
+// create a function to handle deleting a task
+function handleDeleteTask(event) {}
+
+// create a function to handle dropping a task into a new status lane
+function handleDrop(event, ui) {}
+
+// when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+$(document).ready(function () {});
